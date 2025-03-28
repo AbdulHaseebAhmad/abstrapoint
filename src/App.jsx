084106a2e20f1d54535a47f2c6bf258be40b5210
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2025-03-28 03:42:09 +0000

    Deploe

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import Navbar from "./Components/Navbar/Navbar"
-import Footer from "./Sections/Footer/Footer"
-import Hero from "./Sections/Hero"
-import SectionFive from "./Sections/SectionFive/SectionFive"
-import SectionFour from "./Sections/SectionFour/SectionFour"
-import SectionSix from "./Sections/SectionSix/SectionSix"
-import SectionSeven from "./Sections/SectionsSeven/SectionSeven"
-import SectionThree from "./Sections/SectionThree/SectionThree"
-import SectionTwo from "./Sections/SectionTwo/SectionTwo"
- 
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import Home from "./Pages/Home"
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  }
+])
 function App() {
 
   return (
-    <div className="bg-neutralsilver h-screen max-w-[1207px] mx-auto">
-      <Navbar />
-      <Hero/>
-      <SectionTwo/>
-      <SectionFour/>
-      <SectionFive/>
-      <SectionSix/>
-      <SectionSeven/>
-      {/* <Footer/> */}
-
-      {/* <SectionThree/>
-      */}
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
